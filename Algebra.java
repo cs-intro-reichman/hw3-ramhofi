7e7e4e659475dccc1d99c348f114976391eb97e1
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-22 22:08:07 +0200

    try1

--- Algebra.java
+++ Algebra.java
@@ -6,21 +6,21 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+	   // System.out.println(plus(2,3));   // 2 + 3
+	    //System.out.println(minus(7,2));  // 7 - 2
+   		//System.out.println(minus(2,7));  // 2 - 7
+ 		//System.out.println(times(3,4));  // 3 * 4
+   		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		//System.out.println(pow(5,3));      // 5^3
+   		//System.out.println(pow(3,5));      // 3^5
+   		//System.out.println(div(12,3));   // 12 / 3    
+   		//System.out.println(div(5,5));    // 5 / 5  
+   		//System.out.println(div(25,7));   // 25 / 7
+   		//System.out.println(mod(25,7));   // 25 % 7
+   		//System.out.println(mod(120,6));  // 120 % 6    
    		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+		//System.out.println(sqrt(263169));
+   		//System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
@@ -58,7 +58,7 @@ public class Algebra {
 		for(int i = 0; i < n; i++) {
 			x1 = times(x1, x);
 		}
-		return 0;
+		return x1;
 	}
 
 	// Returns the integer part of x1 / x2 
@@ -66,7 +66,7 @@ public class Algebra {
 		// Replace the following statement with your code
 		int count = 0;
 		int x3 = x1;
-		while(x3 != 0) {
+		while(x3 >= x2) {
 			x3 = minus(x3, x2);
 			count++;
 		}
@@ -84,12 +84,12 @@ public class Algebra {
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		// Replace the following statement with your code
-		int i = 1;
+		int i = 0;
 		int x2 = 1;
 		while( x2 != x)
 		{
-			x2 = pow(i,2);
 			i++;
+			x2 = pow(i,2);
 		}
 		return i;
 	}	  	  
