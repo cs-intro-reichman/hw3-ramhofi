aeb6285ae5012b42d31e90b48111f7cd6fae3b0d
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-23 22:12:02 +0200

    try10

--- Algebra.java
+++ Algebra.java
@@ -13,9 +13,9 @@ public class Algebra {
    		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		//System.out.println(pow(5,3));      // 5^3
    		//System.out.println(pow(3,5));      // 3^5
-   		//System.out.println(div(12,3));   // 12 / 3    
-   		//System.out.println(div(5,5));    // 5 / 5  
-   		//System.out.println(div(25,7));   // 25 / 7
+   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(5,5));    // 5 / 5  
+   		System.out.println(div(25,7));   // 25 / 7
    		//System.out.println(mod(25,7));   // 25 % 7
    		//System.out.println(mod(120,6));  // 120 % 6    
    		//System.out.println(sqrt(36));
@@ -99,34 +99,21 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		// Replace the following statement with your code
-		boolean isNegative = false;
-		if(x1 < 0 && x2 > 0) {
-			x1 = minus(0, x1);
-			isNegative = true;
+		if (x2 == 0) {
+			return 0;
 		}
+		boolean isNegative = (x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0);
+
+		x1 = (x1 < 0) ? minus(0, x1) : x1;
+		x2 = (x2 < 0) ? minus(0, x2) : x2;
 		
-		if (x1 > 0 && x2 < 0) {
-			x2 = minus(0, x2);
-			isNegative = true;
-		}
-
 		int count = 0;
-		int x3 = x1;
-		while(x3 >= x2) {
-			x3 = minus(x3, x2);
+		while(x1 >= x2) {
+			x1 = minus(x1, x2);
 			count++;
 		}
 
-		while(x3 <= x2) {
-			x3 = minus(x3, x2);
-			count++;
-		}
-
-			if (isNegative) {
-				count = minus(0, count);				
-			}
-
-		return count;
+		return isNegative ? minus(0, count) : count;
 	}
 
 
