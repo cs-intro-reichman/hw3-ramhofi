3e3bde688d1f7cef9c16676f357480652c05669c
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-23 14:26:00 +0200

    try2

--- Algebra.java
+++ Algebra.java
@@ -26,19 +26,33 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
+		if(x2 > 0) {
 		for(int i = 0; i < x2; i++) {
-			x1++; 
-		}
+			x1++;
+		} 
 		return x1;
+	}
+		else {
+		for(int i = 0; i < -x2; i++) {
+			x1--;
+		}	
+		return x1;
+		}
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
+		if(0 < x2) {
 		for(int i = 0; i < x2; i++) {
 			x1--;
 		}
 		return x1;
+	}
+	else {
+		for(int i = 0; i < x2; i++) {
+			x1++;	
+	}
 	}
 
 	// Returns x1 * x2
